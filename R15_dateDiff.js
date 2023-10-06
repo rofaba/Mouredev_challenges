be487a5bd8c23bcf0deaafa0e9b73ee06ef920a4
rofaba <rodrigofaure@gmail.com>
2023-10-06 16:43:34 -0300

    add validations

--- R15_dateDiff.js
+++ R15_dateDiff.js
@@ -11,18 +11,29 @@ Crea una función que calcule y retorne cuántos días hay entre dos cadenas de 
 
 */
 
-function dateDiff(str1, str2){
+function dateDiff(str1, str2){ 
+    
+    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/
+    
+if(dateRegex.test(str1) && dateRegex.test(str2)){
     let firstdate = str1.split("/")
     let seconddate = str2.split("/")
+    const requirement = (
+        (firstdate[0] > 0 && firstdate[0] < 31) &&  
+        (seconddate[1] > 0 && seconddate[1] < 12)
+        )
+        if (requirement){ 
+            let date1 = new Date(firstdate[2], firstdate[1], firstdate[0])
+            let date2 = new Date(seconddate[2], seconddate[1], seconddate[0])
  
-    let date1 = new Date(firstdate[2], firstdate[1], firstdate[0])
-    let date2 = new Date(seconddate[2], seconddate[1], seconddate[0])
- 
-    let diffabs = Math.abs(date1 - date2)
-    let days = diffabs/(1000*60*60*24)
-    console.log(days)
+            let diffabs = Math.abs(date1 - date2)
+            let days = diffabs/(1000*60*60*24)
+            console.log(days)
+        } else console.log("Fecha no válida")
+
+} else throw "Formato de fecha erroneo, use dd/mm/yyyy"    
 }
 
-dateDiff("27/07/1975", "08/03/1979")
+dateDiff("27/07/1975", "30/08/2013")
 
-dateDiff("27/07/1975", "20/07/1975")
+//dateDiff("08/03/1975", "17/01/2012")
